ca0faf291cce8acec2e52bd29c93a64d561022d3
Juan Bruno MacBook <juan.marcos.bruno@gmail.com>
2016-08-03 23:42:02 +0100

    This commit includes:
    - Addition of two HTTP methods for the crud operations: POST on hello controller to provide a name
    - And DELETE on Costumers controller to delete a costumer

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -1,10 +1,11 @@
 package com.example.controllers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,15 +27,33 @@ public class CustomerController {
 	
 	static {
 		list.add(new Customer(new Long(123), "Juan Bruno"));
-		list.add(new Customer(new Long(123), "Nolwenn Nestic"));
-		list.add(new Customer(new Long(123), "Cristina Barone"));
+		list.add(new Customer(new Long(456), "Nolwenn Nestic"));
+		list.add(new Customer(new Long(789), "Cristina Barone"));
 	}
 	
 	//GET for REST API - CRUD Method = R = Retrieve
-	
+	//We specify the return response that produces and annotated accordingly to use the internal spring objects for conversion
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public List<Customer> getCustomers() {
 		return list;
+	}
+	
+	//POST for REST API - CRUD Method = D = DELETE
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public boolean deleteCustomer(@PathVariable("id") Long id) {
+		Iterator<Customer> listIterator = list.iterator();
+		
+		while(listIterator.hasNext()) {
+			Customer currentCustomer = listIterator.next();
+			
+			if(currentCustomer.getId().equals(id)) {
+				listIterator.remove();
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
--- src/main/java/com/example/controllers/HelloController.java
+++ src/main/java/com/example/controllers/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.controllers;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableAutoConfiguration
@@ -14,5 +17,14 @@ public class HelloController {
 	@ResponseBody
 	public String hello() {
 		return "Hello, World!";
+	}
+	
+	/**Following illustrates use of passed parameters to the HTTP method POST, leaving the controller mapping BUT relying on http method
+	for serving this, specifying the content that consumes and as the body (see param annotation). Not http request param rather
+	whole JSON body here. **/
+	@RequestMapping(value = "/named", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String greetings(@RequestBody String name) {
+		return "Hello " + name;
 	}
 }
